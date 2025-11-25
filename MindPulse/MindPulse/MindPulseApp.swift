5171fd8ba4707b05ddbab166381e33834231ff0f
Adam Hamr <xhamr@mendelu.cz>
2025-11-25 13:48:47 +0100

    Project created

---
+++ MindPulse/MindPulse/MindPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindPulseApp.swift
+//  MindPulse
+//
+//  Created by Adam Hamr on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MindPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
